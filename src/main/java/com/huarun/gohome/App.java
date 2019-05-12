93d79167fccfdf50c5877f82b078c1e0582dce7b
Chen Jian 陈健 <CHENJIAN380@crhd0a.crc.hk>
2019-05-12 14:02:19 +0800

    update

--- src/main/java/com/huarun/gohome/App.java
+++ src/main/java/com/huarun/gohome/App.java
@@ -3,11 +3,30 @@ package com.huarun.gohome;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @MapperScan("com.huarun.gohome.mapper")
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+
+				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/gohome/index.html");
+
+				container.addErrorPages(error404Page);
+			}
+		};
 	}
 }
